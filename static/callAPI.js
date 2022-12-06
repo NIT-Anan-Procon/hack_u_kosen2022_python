97095040f8c6955100c29d33e34b4f2e63d93d45
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-12-07 00:10:01 +0900

    Webページの修正

--- static/callAPI.js
+++ static/callAPI.js
@@ -1,30 +1,32 @@
 const canvas = document.getElementById("canvas");
 
-setInterval(() => {
-  canvas
-    .getContext("2d")
-    .drawImage(
-      video,
-      0,
-      0,
-      canvas.width,
-      canvas.height,
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-  fetch("http://127.0.0.1/api", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      image: canvas.toDataURL().replace(/^data:\w+\/\w+;base64,/, ""),
-    }),
-  })
-    .then(function (response) {
-      return response.json();
+function Test() {
+  setInterval(() => {
+    canvas
+      .getContext("2d")
+      .drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+    fetch("http://202.231.44.30/measure-variable", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        image: canvas.toDataURL().replace(/^data:\w+\/\w+;base64,/, ""),
+      }),
     })
-    .then(function (data) {
-      console.log(data["variable"]);
-    });
-}, 2000);
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data["variable"]);
+      });
+  }, 2000);
+}
