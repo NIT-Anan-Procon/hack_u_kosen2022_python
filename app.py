2859241aef54c532cc5c61191b56027e1592c9a5
KubotaShunsuke <s.kubota0921@gmail.com>
2022-12-12 09:50:25 +0900

    顔を認識するAPIの実装 #2

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 
 from MeasureVariable import MeasureVariable
+from Expression import *
 
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024
@@ -23,6 +24,11 @@ def CallMeasureVariable():
     with open("images/img2.jpg", mode="wb") as file:
         file.write(base64.b64decode(request.json["image"]))
     return MeasureVariable()
+
+@app.route("/face-recognition", methods=["GET"])
+def GetDegreeOfSurpriseFromFace():
+    img_url = "images/img2.jpg"
+    return GetDegreeOfSurprise(img_url)
 
 
 if __name__ == "__main__":
